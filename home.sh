4e21b5e30363e16e021e011d8bcb62c5e1540430
rekkke <rekkke@outlook.com>
2015-04-26 15:34:44 +1000

    Update home.sh

--- home.sh
+++ home.sh
@@ -5,13 +5,14 @@ wget http://javadl.sun.com/webapps/download/AutoDL?BundleId=68233 -O java.rpm
 rpm -Uvh java.rpm
 yum groupinstall "Xfce" "Desktop" -y
 yum install -y nano unzip xkill firefox tigervnc-server xorg-x11-server-Xorg gdm xorg-x11-fonts-* libXtst-devel-*
-clear
-vncserver && vncserver -kill :1
-sed -i 's/twm/startxfce4/g' ~/.vnc/xstartup
-vncserver
 rm -rf *.rpm
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jdk-8u45-linux-x64.rpm"
 
 rpm -i jdk-8u45-linux-x64.rpm
 cd Desktop
 wget https://tribot.org/bin/TRiBot_Loader.jar
+adduser rekke
+passwd rekke
+vncserver && vncserver -kill :1
+sed -i 's/twm/startxfce4/g' ~/.vnc/xstartup
+vncserver
